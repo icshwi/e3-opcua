fc57a6c88c44d89cc54c4611279cc02f07dd2cfe
Jeong Han Lee <jeonghan.lee@gmail.com>
2018-12-03 22:46:45 +0100

    add st3.cmd

--- cmds/st3.cmd
+++ cmds/st3.cmd
@@ -6,7 +6,7 @@ epicsEnvSet("SESSION",   "OPC1")
 epicsEnvSet("SUBSCRIPT", "SUB1")
 epicsEnvSet("OPCSERVER", "127.0.0.1")
 #epicsEnvSet("OPCSERVER", "10.0.6.172")
-loadIocsh("opcua.iocsh", "P=OPC:,SESS=$(SESSION),SUBS=$(SUBSCRIPT),INET=$(OPCSERVER)")
+iocshLoad("$(TOP)/iocsh/opcua.iocsh", "P=OPC:,SESS=$(SESSION),SUBS=$(SUBSCRIPT),INET=$(OPCSERVER)")
 
 dbLoadRecords("$(TOP)/template/UAopcuaDemoServer.template", "P=OPC:,R=,SESS=$(SESSION),SUBS=$(SUBSCRIPT)")
 
