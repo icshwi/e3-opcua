9772221a78e682e1b3cd36d6b06682b9609639e7
Jeong Han Lee <jeonghan.lee@gmail.com>
2018-12-03 14:53:53 +0100

    use ip instead of 127.0.0.1

--- cmds/README.md
+++ cmds/README.md
@@ -5,11 +5,11 @@
 opcua-server (master)$ ./start_server.bash edit
 ```
 
-Replace [gethostname] with 127.0.0.1
+Replace [gethostname] with its ip address
 
 ```
-Endpoints/0/EndpointURL = opc.tcp://127.0.0.1:48020
-Endpoints/0/BindURL = opc.tcp://127.0.0.1:48020
+Endpoints/0/EndpointURL = opc.tcp://10.0.6.172:48020
+Endpoints/0/BindURL = opc.tcp://10.0.6.172:48020
 
 ```
 
--- cmds/st2.cmd
+++ cmds/st2.cmd
@@ -4,8 +4,8 @@ epicsEnvSet(TOP, "$(E3_CMD_TOP)/..")
 
 epicsEnvSet("SESSION",   "OPC1")
 epicsEnvSet("SUBSCRIPT", "SUB1")
-epicsEnvSet("OPCSERVER", "127.0.0.1")
-
+#epicsEnvSet("OPCSERVER", "127.0.0.1")
+epicsEnvSet("OPCSERVER", "10.0.6.172")
 loadIocsh("opcua.iocsh", "P=OPC:,SESS=$(SESSION),SUBS=$(SUBSCRIPT),INET=$(OPCSERVER)")
 
 dbLoadRecords("$(TOP)/template/UAopcuaDemoServer.template", "P=OPC:,R=,SESS=$(SESSION),SUBS=$(SUBSCRIPT)")
