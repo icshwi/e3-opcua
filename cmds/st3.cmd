8e60d0c3c93d64d15d028db081f7a40a9117bd39
Jeong Han Lee <jeonghan.lee@gmail.com>
2019-01-04 18:19:43 +0100

    add the UA opcua Demo Server DB template

--- cmds/st3.cmd
+++ cmds/st3.cmd
@@ -6,7 +6,7 @@ epicsEnvSet("SESSION",   "OPC1")
 epicsEnvSet("SUBSCRIPT", "SUB1")
 epicsEnvSet("OPCSERVER", "127.0.0.1")
 #epicsEnvSet("OPCSERVER", "10.0.6.172")
-iocshLoad("$(TOP)/iocsh/opcua.iocsh", "P=OPC:,SESS=$(SESSION),SUBS=$(SUBSCRIPT),INET=$(OPCSERVER)")
+iocshLoad("$(TOP)/iocsh/opcua.iocsh", "P=OPC:,SESS=$(SESSION),SUBS=$(SUBSCRIPT),INET=$(OPCSERVER), DEBUG=1, SUBSDEBUG=0")
 
 dbLoadRecords("$(TOP)/template/UAopcuaDemoServer.template", "P=OPC:,R=Demo:,SESS=$(SESSION),SUBS=$(SUBSCRIPT)")
 
