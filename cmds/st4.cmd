7f3a42a447d159e3f5649619ec77f9c687f6432d
Jeong Han Lee <jeonghan.lee@gmail.com>
2019-01-04 18:28:08 +0100

    add an additional startup with iocStats

---
+++ cmds/st4.cmd
@@ -0,0 +1,21 @@
+require iocStats,ae5d083
+
+
+require opcua,0.0.0
+
+epicsEnvSet(TOP, "$(E3_CMD_TOP)/..")
+
+epicsEnvSet("SESSION",   "OPC1")
+epicsEnvSet("SUBSCRIPT", "SUB1")
+epicsEnvSet("OPCSERVER", "127.0.0.1")
+#epicsEnvSet("OPCSERVER", "10.0.6.172")
+loadIocsh("iocStats.iocsh", "IOCNAME=$(SESSION):$(SUBSCRIPT)")
+iocshLoad("$(TOP)/iocsh/opcua.iocsh", "P=OPC:,SESS=$(SESSION),SUBS=$(SUBSCRIPT),INET=$(OPCSERVER), DEBUG=1, SUBSDEBUG=0")
+
+dbLoadRecords("$(TOP)/template/UAopcuaDemoServer.template", "P=OPC:,R=Demo:,SESS=$(SESSION),SUBS=$(SUBSCRIPT)")
+
+
+iocInit()
+
+dbl > "$(TOP)/$(SESSION)_PVs.list"
+
